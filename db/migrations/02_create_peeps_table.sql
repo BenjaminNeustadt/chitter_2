5c7d72ecf38d4c4c43124a7f7a560cde96c3b6c8
benjamin <b.james.neustadt@gmail.com>
2022-12-15 22:06:25 +0100

    Connect db in psql locally

---
+++ db/migrations/02_create_peeps_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE peeps(
+  id SERIAL PRIMARY KEY,
+  content VARCHAR(60) NOT NULL,
+  user_id int,
+  time_stamp timestamp NOT NULL DEFAULT NOW(),
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
