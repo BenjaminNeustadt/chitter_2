b51320344b8108afdac89c63cd6987c75d4ecb89
benjamin <b.james.neustadt@gmail.com>
2023-02-19 21:31:06 +0100

    Update db connection for render

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -1,6 +1,21 @@
 require 'pg'
 
 class DatabaseConnection
+  def self.connect
+    # If the environment variable (set by Render)
+    # is present, use this to open the connection.
+    if ENV['DATABASE_URL'] != nil
+      @connection = PG.connect(ENV['DATABASE_URL'])
+      return
+    end
+  
+    if ENV['ENV'] == 'test'
+      database_name = 'chit_test'
+    else
+      database_name = 'chit_chat'
+    end
+    @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
+  end
 
   def self.setup(dbname)
     @connection = PG.connect(dbname: dbname)
